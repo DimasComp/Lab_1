05e94fc307ad930e9bb06adf7ebbc75618f251ab
daniilchubich <daniilchubich@gmail.com>
2024-02-28 22:39:59 +0200

    Formatted code

--- main.go
+++ main.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-    "encoding/json"
-    "net/http"
-    "time"
+	"encoding/json"
+	"net/http"
+	"time"
 )
 
 func main() {
 	port := "8795"
 
-  http.HandleFunc("/time", onTimeRequest)
-  http.ListenAndServe(":" + port, nil)
+	http.HandleFunc("/time", onTimeRequest)
+	http.ListenAndServe(":"+port, nil)
 }
 
 func onTimeRequest(w http.ResponseWriter, r *http.Request) {
-    time := time.Now().Format(time.RFC3339)
+	time := time.Now().Format(time.RFC3339)
 
-    response := map[string]string{"time": time}
+	response := map[string]string{"time": time}
 
-    jsonResponse, err := json.Marshal(response)
-		
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	jsonResponse, err := json.Marshal(response)
 
-    w.Header().Set("Content-Type", "application/json")
-    w.Write(jsonResponse)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonResponse)
 }
